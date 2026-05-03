4512f36939be5ace65cdbb9d3ab7348094834093
Swechha-prob <swechha3424.beai25@chitkara.edu.in>
2026-05-03 15:59:21 +0530

    Initial commit

---
+++ basic_calculator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main{
+    float x,y;
+    printf("Enter num1:");
+    scanf("%f", &x);
+    printf("Enter num2:");
+    scanf("%f", &x);
+    printf("Addition: %f\n", x+y);
+    printf("Subtraction: %f\n", x-y);
+    printf("Multiplication: %f\n", x*y);
+    printf("Division: %f", x/y);
+}
