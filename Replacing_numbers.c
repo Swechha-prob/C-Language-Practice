08544b376a9dfbb4be0c740e34c3485c433ac89f
Swechha-prob <swechha3424.beai25@chitkara.edu.in>
2026-05-03 16:19:39 +0530

    replacing negative numbers with 0

---
+++ Replacing_numbers.c
@@ -0,0 +1,21 @@
+// Replacing negatives numbers with 0
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter n: "); 
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        if (a[i] < 0){
+        a[i] = 0;
+        }
+    }
+    printf("Array after replacing negatives with 0: ");
+    for (int i = 0; i < n; i++){
+        printf("%d ", a[i]);
+    }
+}
