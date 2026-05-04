6ca99ec664ec9c219e312128519dfafa2f716d80
Swechha-prob <swechha3424.beai25@chitkara.edu.in>
2026-05-04 10:08:55 +0530

    min and max in 1 d array

---
+++ MinMax_In_1D_Array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+    int n, min,max;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter elements of array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    max = min = a[0];
+    printf("Even elements:");
+    for (int i = 1; i < n; i++) {
+        if (a[i] < min) {
+            min = a[i];
+        }
+        if (a[i] > max) {
+            max = a[i];
+        }
+        if (a[i]%2 == 0){
+            printf("%d ",a[i]);
+        }
+    }
+    printf("\nMinimum element: %d\n", min);
+    printf("Maximum element: %d", max);
+}
