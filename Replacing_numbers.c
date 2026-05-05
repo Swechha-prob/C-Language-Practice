6ada4e73707693eba0daa87af089bb19345ea9d4
Swechha-prob <swechha3424.beai25@chitkara.edu.in>
2026-05-05 15:13:55 +0530

    return

--- Replacing_numbers.c
+++ Replacing_numbers.c
@@ -18,4 +18,5 @@ int main(){
     for (int i = 0; i < n; i++){
         printf("%d ", a[i]);
     }
+    return 0;
 }
--- Sort_Asc_and_Des_in_1D_Array.c
+++ Sort_Asc_and_Des_in_1D_Array.c
@@ -37,4 +37,5 @@ int main(){
     for (int i = 0; i < n; i++) {
         printf("%d ", a[i]);
     }
+    return 0;
 }
