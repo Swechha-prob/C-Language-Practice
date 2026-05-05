70bf8ddd6362350e399336229f8758ae9e9111e4
Swechha-prob <swechha3424.beai25@chitkara.edu.in>
2026-05-05 14:28:04 +0530

    sort 1D array in both ascending and descending array

---
+++ Sort_Asc_and_Des_in_1D_Array.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter n: "); 
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    // Ascending
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (a[i] > a[j]) {
+                int t = a[i];
+                a[i] = a[j];
+                a[j] = t;
+            }
+        }
+    }
+    printf("Ascending: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    // Descending
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (a[i] < a[j]) {
+                int t = a[i];
+                a[i] = a[j];
+                a[j] = t;
+            }
+        }
+    }
+    printf("Descending: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+}
